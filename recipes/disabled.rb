721ecb0f23e4a566264aa744f5cd7855974a9491
Bryan Berry <bryan.berry@gmail.com>
2011-10-16 12:34:21 +0200

    Fixed trailing commas. knife cookbook upload selinux fails if not removed

--- recipes/disabled.rb
+++ recipes/disabled.rb
@@ -18,20 +18,16 @@
 # limitations under the License.
 #
 
-
-include_recipe "selinux-monkeys::default"
-
 execute "disable selinux enforcement" do
   only_if "selinuxenabled"
   command "setenforce 0"
   action :run
 end
 
-template "/etc/sysconfig/selinux" do
+template "/etc/selinux/config" do
   source "sysconfig/selinux.erb"
   variables(
     :selinux => "disabled",
-    :selinuxtype => "targeted",
+    :selinuxtype => "targeted"
   )
 end
-
